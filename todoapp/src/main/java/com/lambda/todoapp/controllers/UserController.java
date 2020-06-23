8fad91af016a2921d9fbfe7cc9f238dc9a24986a
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-23 14:23:49 +0800

    Add POST '/users/user' route

--- todoapp/src/main/java/com/lambda/todoapp/controllers/UserController.java
+++ todoapp/src/main/java/com/lambda/todoapp/controllers/UserController.java
@@ -3,13 +3,14 @@ package com.lambda.todoapp.controllers;
 import com.lambda.todoapp.models.User;
 import com.lambda.todoapp.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -51,4 +52,21 @@ public class UserController {
         
         return new ResponseEntity<>(u, HttpStatus.OK);
     }
+
+    @PostMapping(value = "/user", consumes = {"application/json"})
+    public ResponseEntity<?> addUser(@Valid @RequestBody User newUser) {
+        newUser.setUserid(0);
+        newUser = userService.save(newUser);
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{userid}")
+                .buildAndExpand(newUser.getUserid())
+                .toUri();
+
+        responseHeaders.setLocation(newUserURI);
+
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
 }

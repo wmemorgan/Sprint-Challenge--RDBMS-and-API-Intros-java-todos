e2974b78f97ef55c81db077cb084085b91b27dfa
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-23 12:00:51 +0800

    Create User model

---
+++ todoapp/src/main/java/com/lambda/todoapp/models/User.java
@@ -0,0 +1,98 @@
+package com.lambda.todoapp.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The entity allowing interaction with the Users table
+ */
+@Entity
+@Table(name = "users")
+public class User extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long userid;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String primaryemail;
+
+    @Column(nullable = false)
+    private String password;
+
+    /**
+     * Creates a join table joining Users and Todos in a One-To-Many relation.
+     * Contains a List of Todo objects associated with this user.
+     */
+    @OneToMany(mappedBy = "user",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user")
+    private List<Todo> todos = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String username, String primaryemail, String password) {
+        this.username = username;
+        this.primaryemail = primaryemail;
+        this.password = password;
+    }
+
+    public long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(long userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPrimaryemail() {
+        return primaryemail;
+    }
+
+    public void setPrimaryemail(String primaryemail) {
+        this.primaryemail = primaryemail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Todo> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(List<Todo> todos) {
+        this.todos = todos;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userid=" + userid +
+                ", username='" + username + '\'' +
+                ", primaryemail='" + primaryemail + '\'' +
+                ", password='" + password + '\'' +
+                ", todos=" + todos +
+                '}';
+    }
+}

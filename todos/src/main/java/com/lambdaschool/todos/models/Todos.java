e5bf770516a846600b2d9f081ac0fa0882340ca7
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-17 15:15:00 +0800

    Create Todos data model

---
+++ todos/src/main/java/com/lambdaschool/todos/models/Todos.java
@@ -0,0 +1,63 @@
+package com.lambdaschool.todos.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class Todos extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long todoid;
+
+    private String description;
+
+    private boolean completed = false;
+
+    @ManyToOne
+    @JoinColumn(name = "userid", nullable = false)
+    @JsonIgnoreProperties(value = "todos")
+    private User user;
+
+    public Todos() {
+    }
+
+    public Todos(User user, String description) {
+        this.user = user;
+        this.description = description;
+    }
+
+    public long getTodoid() {
+        return todoid;
+    }
+
+    public void setTodoid(long todoid) {
+        this.todoid = todoid;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
